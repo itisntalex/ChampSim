4a305116f016164a7a5318529286742d862e03fc
Alexandre V. Jamet <alexandre.jamet@bsc.es>
2019-03-28 11:26:01 +0100

    Adding the Multiperspective Reuse Prediction for comparison purpose.

---
+++ src/replacement/multiperspective/multiperspective.cmake
@@ -0,0 +1,4 @@
+# Getting the MPPPB-related source files.
+file(GLOB_RECURSE CHAMPSIM_REPLACEMENT_POLICY_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/src/replacement/multiperspective/*.cc)
+
+list(APPEND CHAMPSIM_REPLACEMENT_POLICY_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/src/replacement/base_replacement.cc)
